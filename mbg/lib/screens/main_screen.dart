68e36cbef02a33370f05c1f7a69f6e9daeceddff
yaaqas123 <yazidaqilassalam04@gmail.com>
2025-05-19 00:22:08 +0700

    up

--- mbg/lib/screens/main_screen.dart
+++ mbg/lib/screens/main_screen.dart
@@ -9,59 +9,37 @@ import 'orangtua/orangtua_dashboard.dart';
 import 'dinas/dinas_dashboard.dart';
 import 'katering/katering_dashboard.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final userRole = Provider.of<UserProvider>(context).role;
 
-    List<Widget> pages;
+    Widget page;
 
     switch (userRole) {
       case 'Admin Sekolah':
-        pages = [const AdminDashboard(), Placeholder(), Placeholder()];
+        page = const AdminDashboard();
         break;
       case 'Guru':
-        pages = [const GuruDashboard(), Placeholder(), Placeholder()];
+        page = const GuruDashboard();
         break;
       case 'Orang Tua':
-        pages = [const OrangTuaDashboard(), Placeholder(), Placeholder()];
+        page = const OrangTuaDashboard();
         break;
       case 'Dinas Pendidikan':
-        pages = [const DinasDashboard(), Placeholder(), Placeholder()];
+        page = const DinasDashboard();
         break;
       case 'Tim Katering':
-        pages = [const KateringDashboard(), Placeholder(), Placeholder()];
+        page = const KateringDashboard();
         break;
       default:
-        pages = [const Center(child: Text("Role tidak dikenali"))];
+        page = const Center(child: Text("Role tidak dikenali"));
     }
 
     return Scaffold(
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Beranda"),
-          BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: "Fitur"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Lainnya"),
-        ],
-      ),
+      body: page,
     );
   }
 }
