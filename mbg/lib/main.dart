16ae1829c29afe9f9c940a3b7f43f82870a80846
yaaqas123 <yazidaqilassalam04@gmail.com>
2025-06-02 21:28:05 +0700

    up

--- mbg/lib/main.dart
+++ mbg/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'provider/user_provider.dart';
 import 'theme/app_theme.dart';
 import 'Login/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
+
   runApp(
     MultiProvider(
       providers: [
