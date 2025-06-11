32384d338029f6232a44cd199130d484122ccf32
yaaqas123 <yazidaqilassalam04@gmail.com>
2025-06-11 10:59:10 +0700

    up

--- mbg/lib/main.dart
+++ mbg/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'provider/user_provider.dart';
 import 'theme/app_theme.dart';
 import 'splash_screen1.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}
+  await initializeDateFormatting('id_ID', null);
 
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
